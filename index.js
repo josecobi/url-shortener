eb18542b54530e56fd9557084d7c2205fdcb3c27
josecobi <58313777+josecobi@users.noreply.github.com>
2024-01-11 15:10:25 -0500

    Created variables for DOM elements

---
+++ index.js
@@ -0,0 +1,4 @@
+// Create variables for DOM elements
+const form = document.querySelector(".form");
+const inputBox = document.querySelector(".urlInputBox");
+const shortenBtn = document.querySelector(".shortenBtn");
