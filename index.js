08a3e551a4dafabfb86daa741efbeb9f08f154b1
josecobi <58313777+josecobi@users.noreply.github.com>
2024-01-11 15:29:37 -0500

    created function to get the link submitted by the user

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 const form = document.querySelector(".form");
 const inputBox = document.querySelector(".urlInputBox");
 const shortenBtn = document.querySelector(".shortenBtn");
+
+// Declare a function that gets the link the user submitted
+function getLongLink(){
+    form.addEventListener("submit", (event) =>{
+        event.preventDefault();
+        console.log(inputBox.value);
+        return inputBox.value;
+    });
+}
+
+getLongLink();
